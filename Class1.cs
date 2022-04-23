07007ff537acdfb5f48e0c5f47152bf650f8150d
Gangula2 <gangulaanwesh@gmail.com>
2022-04-23 15:48:17 +0530

    Added basic plugin to get the bounds of a rectangle + ReadMe

---
+++ Class1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.Runtime;
+using ACAD = Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using System.Windows.Forms;
+
+namespace AutocadAPI
+{
+    public class Class1
+    {
+        [CommandMethod("AdskGreeting")]
+        public void AdskGreeting()
+        {
+            // Get the current document and database, and start a transaction
+            ACAD.Document acDoc = ACAD.Core.Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Starts a new transaction with the Transaction Manager
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Block table record for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
+                                             OpenMode.ForRead) as BlockTable;
+
+                // Open the Block table record Model space for write
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+                                                OpenMode.ForWrite) as BlockTableRecord;
+
+                /* Creates a new MText object and assigns it a location,
+                text value and text style */
+                MText objText = new MText();
+
+                // Set the default properties for the MText object
+                objText.SetDatabaseDefaults();
+
+                // Specify the insertion point of the MText object
+                objText.Location = new Autodesk.AutoCAD.Geometry.Point3d(2, 2, 0);
+
+                // Set the text string for the MText object
+                objText.Contents = "Greetings, Welcome to the AutoCAD .NET Developer's Guide";
+
+                // Set the text style for the MText object
+                objText.TextStyleId = acCurDb.Textstyle;
+
+                // Appends the new MText object to model space
+                acBlkTblRec.AppendEntity(objText);
+
+                // Appends to new MText object to the active transaction
+                acTrans.AddNewlyCreatedDBObject(objText, true);
+
+                // Saves the changes to the database and closes the transaction
+                acTrans.Commit();
+            }
+        }
+
+        [CommandMethod("SelectObjectsOnscreen")]
+        public static void SelectObjectsOnscreen()
+        {
+            // Get the current document and database
+            ACAD.Document acDoc = ACAD.Core.Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Request for objects to be selected in the drawing area
+                PromptSelectionResult acSSPrompt = acDoc.Editor.GetSelection();
+
+                // If the prompt status is OK, objects were selected
+                if (acSSPrompt.Status == PromptStatus.OK)
+                {
+                    SelectionSet acSSet = acSSPrompt.Value;
+
+                    // Step through the objects in the selection set
+                    foreach (SelectedObject acSSObj in acSSet)
+                    {
+                        // Check to make sure a valid SelectedObject object was returned
+                        if (acSSObj != null)
+                        {
+                            // Open the selected object for write
+                            Entity acEnt = acTrans.GetObject(acSSObj.ObjectId,
+                                                             OpenMode.ForWrite) as Entity;
+
+                            if (acEnt != null)
+                            {
+                                // Change the object's color to Green
+                                acEnt.ColorIndex = 3;
+                            }
+
+                            Extents3d rectBounds = (Extents3d)acEnt.Bounds;
+                            var MaxPoint = rectBounds.MaxPoint;
+                            var MinPoint = rectBounds.MinPoint;
+
+                            MessageBox.Show("Max Point: X: " + MaxPoint.X.ToString() + " Y: " + MaxPoint.Y.ToString() + "\n" 
+                                + "Min Point: X: " + MinPoint.X.ToString() + " Y: " + MinPoint.Y.ToString());
+                        }
+                    }
+
+                    // Save the new object to the database
+                    acTrans.Commit();
+                }
+
+                // Dispose of the transaction
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # AutoCAD-101
 Basic AutoCAD plugin using C# .NET 
+
+Steps to Run the Code:
+
+1. Open the project (.csproj) in Visual Studio
+2. Click "Build Solution" under Build menu in ribbon (Ctrl+Shift+B)
+3. "bin\debug" folder will be create under you root directory
+4. Make sure that the "bin\debug" folder contains a ".dll" file with the name of your project.
+5. Open AutoCAD and create a new blank drawing
+6. Type in "NETLOAD" command which lets you load the plugin you build above in step 4
+7. Choose the ".dll" file from the "bin\debug" folder
+8. Hit Open - the plugin will be loaded
+
+Test the Plugin:
+1. Draw a Rectange of any dimension
+2. Type "SelectObjectsOnScreen" command in AutoCAD and hit enter
+3. Choose the Rectange that you drew in step 1 & hit enter
+4. The Bounds of the rectangle will be displayed in a pop-up.
